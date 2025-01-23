a36dd47c7db96ec6c675f9f1ba2311a4b8936040
donghwikim-create <cvasq2103@gmail.com>
2025-01-23 10:12:59 +0900

    commit test2

---
+++ src/main/java/com/goingupdragon/going_up_dragon/GoingUpDragonApplication.java
@@ -0,0 +1,13 @@
+package com.goingupdragon.going_up_dragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoingUpDragonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoingUpDragonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goingupdragon/going_up_dragon/GoingUpDragonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goingupdragon.going_up_dragon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoingUpDragonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
