934a56d6ca83b6f50198622470b8a7cce9c56065
donghwikim-create <cvasq2103@gmail.com>
2025-01-23 09:50:20 +0900

    commit test

---
+++ src/main/java/com/goingupdragon/going_up_dragon/controller/DemoController.java
@@ -0,0 +1,8 @@
+package com.goingupdragon.going_up_dragon.controller;
+
+public class DemoController {
+    public static void main(String[] args) {
+        System.out.println("11");
+        System.out.println("123");
+    }
+}
