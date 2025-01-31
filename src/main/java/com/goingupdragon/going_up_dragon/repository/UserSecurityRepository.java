6194873467f6a80286e4ec1a4ec0d9cff5113e11
donghwikim-create <cvasq2103@gmail.com>
2025-01-31 09:45:55 +0900

    중복된 코드 정리

--- src/main/java/com/goingupdragon/going_up_dragon/repository/UserSecurityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.goingupdragon.going_up_dragon.repository;
-
-import com.goingupdragon.going_up_dragon.entity.UserSecurity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserSecurityRepository extends JpaRepository<UserSecurity, Long> {
-}
