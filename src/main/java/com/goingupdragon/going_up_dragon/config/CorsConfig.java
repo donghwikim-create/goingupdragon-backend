4dd2b712953f75735b4492b421d9415538f641d8
clzks <clzks@naver.com>
2025-01-31 14:03:23 +0900

    CorsConfig 추가

---
+++ src/main/java/com/goingupdragon/going_up_dragon/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.goingupdragon.going_up_dragon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // ✅ 모든 엔드포인트 허용
+                        .allowedOrigins("http://localhost:3000") // ✅ React 허용
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
