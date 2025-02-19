b561f26eaf18508fef51cb6c0e8a0f4f0fa37e97
donghwikim-create <cvasq2103@gmail.com>
2025-02-19 15:36:15 +0900

    회원가입 시 프론트 알림창에 --> 회원가입 실패: Unexpected token '회', "회원가입 성공!" is not valid JSON 오류 뜨는 문제 해결

--- src/main/java/com/goingupdragon/going_up_dragon/controller/SignupController.java
+++ src/main/java/com/goingupdragon/going_up_dragon/controller/SignupController.java
@@ -2,11 +2,14 @@ package com.goingupdragon.going_up_dragon.controller;
 
 import com.goingupdragon.going_up_dragon.dto.SignupRequestDTO;
 import com.goingupdragon.going_up_dragon.service.SignupService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api")
@@ -19,8 +22,18 @@ public class SignupController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody SignupRequestDTO request) {
-        signupService.registerUser(request.getEmail(), request.getPassword(), request.getPhoneNumber());
-        return ResponseEntity.ok("회원가입 성공!");
+    public ResponseEntity<Object> signup(@RequestBody SignupRequestDTO request) {
+        try {
+            signupService.registerUser(request.getEmail(), request.getPassword(), request.getPhoneNumber());
+            // JSON 형태로 메시지를 반환
+            return ResponseEntity.ok(new HashMap<String, String>() {{
+                put("message", "회원가입 성공!");
+            }});
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new HashMap<String, String>() {{
+                        put("error", "회원가입 실패: " + e.getMessage());
+                    }});
+        }
     }
 }
